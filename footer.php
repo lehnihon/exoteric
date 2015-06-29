c44aef5d1b2360752635acd335359f852220f673
Leandro <lehnihon@hotmail.com>
2015-06-29 00:56:36 -0300

    página home feita

---
+++ footer.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id #maincontentcontainer div and all content after.
+ * There are also four footer widgets displayed. These will be displayed from
+ * one to four columns, depending on how many widgets are active.
+ *
+ * @package Quark
+ * @since Quark 1.0
+ */
+?>
+
+		<?php	do_action( 'quark_after_woocommerce' ); ?>
+	</div> <!-- /#maincontentcontainer -->
+
+	<div id="footercontainer">
+
+		<footer class="site-footer row" role="contentinfo">
+			<div class="col grid_4_of_12">
+				<img class="efeito-cinza" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/logo_exoteric.png"; ?>" alt="" />
+				<h5>Massagem no Centro de São Paulo</h5>
+				<p>
+					A Clinic Exoteric é uma clínica de massagem no Centro de São Paulo.<br/>
+					Somos especializados em massagens terapêuticas, tântricas, Mix Massagem, Shiatsu, Quick Massagem, Acumputura e todos os tipos de massagem.<br/>
+					Também oferecemos Curso de Massagem.
+				</p>
+			</div>		
+			<div class="col grid_3_of_12">
+				<h5>Massagens</h5>
+				<ul>
+				    <?php
+
+				        $args = array( 'post_type' => 'product', 'posts_per_page' => 8, 'product_cat' => 'categoria', 'orderby' => 'rand' );
+				        $loop = new WP_Query( $args );
+
+				        while ( $loop->have_posts() ) : $loop->the_post(); global $product; 
+				    ?>
+		                <li>   
+		                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+		                        <?php the_title(); ?>                                    
+		                    </a>
+		                  
+		                </li>
+				    <?php endwhile; ?>
+				    <?php wp_reset_query(); ?>
+				</ul>
+			</div>
+			<div class="col grid_3_of_12">
+				<h5>Cursos</h5>
+				<ul>
+				    <?php
+
+				        $args = array( 'post_type' => 'product', 'posts_per_page' => 8, 'product_cat' => 'categoria', 'orderby' => 'rand' );
+				        $loop = new WP_Query( $args );
+
+				        while ( $loop->have_posts() ) : $loop->the_post(); global $product; 
+				    ?>
+		                <li>   
+		                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+		                        <?php the_title(); ?>                                    
+		                    </a>
+		                  
+		                </li>
+				    <?php endwhile; ?>
+				    <?php wp_reset_query(); ?>
+				</ul>
+			</div>
+			<div class="col grid_2_of_12">
+				<h5>Atendimento</h5>
+				<ul> 
+	                <li>   
+	                    <a href="#">Localização</a>
+	                </li>
+	                <li>   
+	                    <a href="#">Contato</a>
+	                </li>
+	                <li><a>11 3203-1478</a></li>
+	                <li><a>11 98937-2110</a></li>	           
+				</ul>
+			</div>
+		</footer> <!-- /.site-footer.row -->
+
+
+	</div> <!-- /.footercontainer -->
+	<div id="formas-pgto">
+		<footer class="site-footer row" style="padding:20px; 0">
+			<div class="col grid_12_of_12">
+				<h5 style="color:#505050">Formas de Pagamento</h5>
+				<img class="efeito-cinza" src="<?php echo dirname( get_bloginfo('stylesheet_url'))."/images/formas_pgto.jpg"; ?>" alt="" />
+			</div>
+		</footer>
+	</div>
+	<div id="copyrightcontainer">
+		<p>
+			© Copyright 2015 - Haishop
+		</p>
+	</div>
+</div> <!-- /.#wrapper.hfeed.site -->
+
+<?php wp_footer(); ?>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Quark
+ * @since Quark 1.0
+ */
+
+get_header( 'shop' ); ?>
+
+	<div id="primary" class="site-content" role="main">
+		<div id="bannercontainer" class="row">
+			<div class="col grid_12_of_12 efeito-cinza">
+				<?php 
+				    echo do_shortcode("[metaslider id=28]"); 
+				?>	
+			</div>			
+		</div> <!-- /#bannercontainer -->
+	</div> <!-- /#primary.site-content.row -->
+	<div id="secondary">
+		<div class="site-content">
+			<h3>MASSAGENS</h3>
+			<div class="row">
+			    <?php
+
+			        $args = array( 'post_type' => 'product', 'posts_per_page' => 3, 'product_cat' => 'categoria', 'orderby' => 'rand' );
+			        $loop = new WP_Query( $args );
+
+			        while ( $loop->have_posts() ) : $loop->the_post(); global $product; 
+			    ?>
+	                <div class="col grid_4_of_12">    
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+	                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID ); else echo '<img src="'.woocommerce_placeholder_img_src().'" />'; ?>	                                           
+	                    </a>
+	                    <h4><?php the_title(); ?></h4>
+	                    <?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>" class="saiba-mais">
+	                    	saiba+
+	                    </a>
+	                </div>
+			    <?php endwhile; ?>
+			    <?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</div>
+	<div id="tertiary">
+		<div class="site-content">
+			<h3>Terapeutas</h3>
+			<?php
+			$args = array( 'posts_per_page' => 6, 'offset'=> 0, 'category_name' => 'terapeutas' );
+
+			$myposts = get_posts( $args );
+			$i = 0;
+			foreach ( $myposts as $post ) : setup_postdata( $post );
+			?>
+				<?php
+				if($i % 3 == 0):
+				?>
+					<div class="row">	
+				<?php endif ?>  
+
+					<div class="col grid_4_of_12">
+						<?php if ( has_post_thumbnail()) : ?>
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" >
+								<?php the_post_thumbnail(); ?>
+							</a>
+						<?php endif; ?>
+						<h4><?php the_title();?></h4>
+	                    <a href="<?php the_permalink(); ?>" class="saiba-mais">
+	                    	saiba+
+	                    </a>
+	            	</div>
+
+	            <?php 
+	            $i++;
+	            if($i % 3 == 0): 
+	            ?>
+					</div>
+				<?php endif ?>
+			<?php 
+			endforeach; 
+			wp_reset_postdata();?>
+		</div>
+	</div>
+	<div id="quaternary">
+		<div class="site-content">
+			<h3>ESTÉTICA</h3>
+			<div class="row">
+			    <?php
+
+			        $args = array( 'post_type' => 'product', 'posts_per_page' => 3, 'product_cat' => 'categoria', 'orderby' => 'rand' );
+			        $loop = new WP_Query( $args );
+
+			        while ( $loop->have_posts() ) : $loop->the_post(); global $product; 
+			    ?>
+	                <div class="col grid_4_of_12">    
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+	                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID ); else echo '<img src="'.woocommerce_placeholder_img_src().'" />'; ?>	                                           
+	                    </a>
+	                    <h4><?php the_title(); ?></h4>
+	                    <?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>" class="saiba-mais">
+	                    	saiba+
+	                    </a>
+	                </div>
+			    <?php endwhile; ?>
+			    <?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</div>
+	<div id="quinary">
+		<div class="site-content">
+			<h3>CURSOS</h3>
+			<div class="row">
+			    <?php
+
+			        $args = array( 'post_type' => 'product', 'posts_per_page' => 3, 'product_cat' => 'categoria', 'orderby' => 'rand' );
+			        $loop = new WP_Query( $args );
+
+			        while ( $loop->have_posts() ) : $loop->the_post(); global $product; 
+			    ?>
+	                <div class="col grid_4_of_12">    
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>">
+	                        <?php if (has_post_thumbnail( $loop->post->ID )) echo get_the_post_thumbnail($loop->post->ID ); else echo '<img src="'.woocommerce_placeholder_img_src().'" />'; ?>	                                           
+	                    </a>
+	                    <h4><?php the_title(); ?></h4>
+	                    <?php echo apply_filters( 'woocommerce_short_description', $post->post_excerpt ) ?>
+	                    <a href="<?php echo get_permalink( $loop->post->ID ) ?>" title="<?php echo esc_attr($loop->post->post_title ? $loop->post->post_title : $loop->post->ID); ?>" class="saiba-mais">
+	                    	saiba+
+	                    </a>
+	                </div>
+			    <?php endwhile; ?>
+			    <?php wp_reset_query(); ?>
+			</div>
+		</div>
+	</div>
+	<div id="newsletter-background">
+		<div class="site-content" style="padding:0">
+			<?php get_template_part( 'newsletter' ); ?>
+		</div>
+	</div>
+	<ul class="redes_sociais_bot">
+		<li class="youtube"><a target="_blank" href="https://www.youtube.com/channel/UCNkHrw4ujO_bUW7nmrPF1TA"></a></li>
+		<li class="instagram"><a target="_blank" href="https://instagram.com/henjioficial"></a></li>
+		<li class="twitter"><a target="_blank" href="https://twitter.com/henjioficial"></a></li>
+		<li class="facebook"><a target="_blank" href="https://www.facebook.com/henjioficial"></a></li>
+	</ul>	
+<?php get_footer(); ?>
